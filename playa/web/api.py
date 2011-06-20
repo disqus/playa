ccf2e7ee43ec25ebf2ce4f516ed62b9332e1db62
David Cramer <dcramer@gmail.com>
2011-06-20 07:11:45 +0200

    Standardize API methods

--- playa/web/api.py
+++ playa/web/api.py
@@ -1,30 +1,41 @@
 from playa import app
 from playa.web.helpers import get_now_playing
 
-from flask import request
+from flask import request, redirect, url_for
 
+import functools
 import simplejson
 
+def api(func):
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        resp = func(*args, **kwargs)
+        if request.is_xhr:
+            return redirect(url_for('index'))
+        return simplejson.dumps(resp or {})
+    return wrapped
+
 @app.route('/api/now_playing', methods=['GET'])
+@api
 def now_playing():
-    return simplejson.dumps(get_now_playing())
+    return get_now_playing()
 
 @app.route('/api/set_volume', methods=['POST'])
+@api
 def set_volume():
     value = request.form['value']
     app.player.set_volume(value)
-    return simplejson.dumps({
+    return {
         'value': app.player.get_volume()
-    })
+    }
 
 @app.route('/api/seek', methods=['POST'])
+@api
 def seek():
     pos = request.form['pos']
     app.player.seek(pos)
-    return simplejson.dumps({})
 
 @app.route('/api/shuffle_all', methods=['POST'])
+@api
 def shuffle_all():
     app.player.shuffle_all()
-    
-    return redirect('/')
